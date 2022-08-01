e73185f667f11c812a196fdeb144f683fdb74ddd
Hassan Refaat <76957887+hassanrefaat9@users.noreply.github.com>
2022-08-01 10:40:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/spotifyclone/SpotifyApplication.kt
@@ -0,0 +1,8 @@
+package com.example.spotifyclone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpotifyApplication :Application() {
+}
---
+++ app/src/main/java/com/example/spotifyclone/other/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.spotifyclone.other
+
+object Constants {
+    const val SONG_COLLECTION = "songs"
+}
