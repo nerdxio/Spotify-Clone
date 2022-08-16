7aa2b2ccccfaaea54e50a8fa7c063f3fef05d738
Hassan Refaat <76957887+hassanrefaat9@users.noreply.github.com>
2022-08-16 13:34:36 +0200

    song view model imp

--- app/src/main/java/com/example/spotifyclone/other/Constants.kt
+++ app/src/main/java/com/example/spotifyclone/other/Constants.kt
@@ -6,5 +6,6 @@ object Constants {
     const val NOTIFICATION_CHANNEL_ID = "music"
     const val MEDIA_ROOT_ID = "root_id"
     const val NETWORK_ERROR = "NETWORK_ERROR"
+    const val UPDATE_PLAYER_POSITION_INTERVAL = 100L
 
 }
